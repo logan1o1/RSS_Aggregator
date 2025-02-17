9604aea45850d4e7c101fe484f480c25fce812f2
Saswat Mahapatra <92862073+logan1o1@users.noreply.github.com>
2025-02-17 22:13:43 +0530

    minor changes to getUser api

--- handler_user.go
+++ handler_user.go
@@ -38,7 +38,27 @@ func (apiCfg *apiConfig) handlerCreateUser(resp http.ResponseWriter, req *http.R
 	respondWithJSON(resp, 201, databaseUserToUser(user))
 }
 
-func (apiCfg *apiConfig) handlerGetUser(resp http.ResponseWriter, req *http.Request, user database.User) {
+// user database.User
+
+func (apiCfg *apiConfig) handlerGetUser(resp http.ResponseWriter, req *http.Request) {
+	type parameters struct {
+		ApiKey string `json:"api_key"`
+	}
+	decoder := json.NewDecoder(req.Body)
+
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(resp, 400, fmt.Sprintf("Err parsing json in get_user_handler: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUsersByAPIKey(req.Context(), params.ApiKey)
+	if err != nil {
+		respondWithError(resp, 400, fmt.Sprintf("Err getting user in get_user_handler: %v", err))
+		return
+	}
+
 	respondWithJSON(resp, 200, databaseUserToUser(user))
 }
 
--- main.go
+++ main.go
@@ -67,7 +67,7 @@ func main() {
 	v1Router.Get("/ready", handlerReadiness)
 	v1Router.Get("/err", handlerError)
 	v1Router.Post("/users", apiCfg.handlerCreateUser)
-	v1Router.Get("/users", apiCfg.authMiddleware(apiCfg.handlerGetUser))
+	v1Router.Post("/users", apiCfg.handlerGetUser)
 	v1Router.Post("/feeds", apiCfg.authMiddleware(apiCfg.handlerCreateFeed))
 	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
 	v1Router.Post("/feed_follows", apiCfg.authMiddleware(apiCfg.handlerCreateFeedFollows))
